139644bddff6383811187dea23edce3c820ad54b
kdmurray <kdmurray@gmail.com>
2021-02-13 20:13:48 -0800

    Initial commit

---
+++ Lutrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lutrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
